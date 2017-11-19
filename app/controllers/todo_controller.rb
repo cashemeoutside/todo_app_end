4fd8b4ce5041fc264496486cd068317401632eea
andres martin <andres.martin@ousd.us>
2017-11-19 22:18:13 +0000

    added edit,update,destroy options

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,10 +2,17 @@ class TodoController < ApplicationController
  def index
  end
  def show
-  todo_id = params[:id]
-  
-  if todo_id == '1'
-   
-  end
+ end
+ def new
+ end
+ def create
+  t = Todo.new
+  t.description = params['description']
+  t.pomodoro_estimate = params['pomodoro_estimate']
+  t.save
+  redirect_to "/todo/show/#{ t.id }"
+ end
+ def edit
+  @todo = Todo.find_by_id(params[:id])
  end
 end
